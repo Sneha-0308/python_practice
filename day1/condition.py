e7ee3dec39d6dcd523b7dceb1fd7246374e27405
Sneha Mathadawar <mathadawar01@gmail.com>
2021-09-30 20:12:46 +0530

    Modify condition.py file

---
+++ day1/condition.py
@@ -0,0 +1,13 @@
+# IF CONDITION
+i = 5
+if i > 3:
+    print("Hey xyz")
+
+# CHECK GIVEN TWO NUMBER IS EVEN OR NOT
+n = 10
+m = 12
+if n % 2 == 0 and m % 2 == 0:
+    print("both number are even")
+else:
+    print("anyone number is not even")
+
